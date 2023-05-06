87dd9375a2fd95f45c87d5841ba333c3606a37b5
annagaom <122282732+annagaom@users.noreply.github.com>
2023-05-06 18:03:49 +0300

    Add files via upload

---
+++ server/rule.py
@@ -0,0 +1,10 @@
+text = "    - Yhteensä on 31 lentokenttää, joista voit etsimään timanttia.\n\
+    - Saat 1000Km ja 15 lentomäärää, millä voit alka.\n\
+    - Jokaisessa lentokentällä, ilmoitetaan etäisyys timantista. \n\
+    - Jokaisen lennon jälkeen tulee ehto, voitaa lisää km tehtävän tekemisen avulla tai jatka matkaa.\n\
+    - Saat 200km kun vastaus on oikein.\n\
+    - Väärällä  vastaulsella vähenetään 50km.\n\
+    - Peli loppuu kun kilometrimäärät loppuvat kesken tai lentänyt 15 kertaa."
+
+def play_rules():
+    print(f"\033[35m{text}\033[0m")
